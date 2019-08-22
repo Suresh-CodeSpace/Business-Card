4c13e224f1236e88c331d3ec4daf904aa52c1e6f
unknown <suresh1010sharma@gmail.com>
2019-08-22 21:06:17 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/Photo.JPG'),
+              ),
+              Text(
+                'Suresh',
+                style: TextStyle(
+                  fontFamily: 'Satisfy',
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.blue.shade200,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 200,
+                child: Divider(
+                  color: Colors.blue.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(Icons.call, color: Colors.blue),
+                  title: Text(
+                    '+91 9876543210',
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.blue.shade600,
+                        fontFamily: 'Source Sans Pro ',
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(Icons.mail, color: Colors.blue.shade600),
+                  title: Text(
+                    'suresh@gmail.com',
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.blue.shade600,
+                        fontFamily: 'Source Sans Pro ',
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
